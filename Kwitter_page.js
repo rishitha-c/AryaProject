a58a629146f409944281238758983b3d5c260c75
rishitha-c <89968312+rishitha-c@users.noreply.github.com>
2021-11-10 12:41:49 +0530

    Add files via upload

--- Kwitter_page.js
+++ Kwitter_page.js
@@ -14,18 +14,18 @@ const firebaseConfig = {
   user_name= localStorage.getItem("user_name") ;
     room_name= localStorage.getItem("room_name") ;
     document.getElementById("room_name").innerHTML="The room is :"+room_name;
-
-function send(){
-     msg = document.getElementById("msg").value;
-
-     firebase.database().ref(room_name).push({
-           name:user_name,
-           message:msg,
-           like:0
-     });
-
-     document.getElementById("msg").value="";
-}
+    
+    function send()
+    {
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+        name:user_name,
+        message:msg,
+        like:0
+       });
+    
+      document.getElementById("msg").value = "";
+    }
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
   firebase_message_id = childKey;
